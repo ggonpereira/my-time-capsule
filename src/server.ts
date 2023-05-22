120d0d59f929a108b4bd19711faa831788c766c8
Gabriel Pereira <ggonpereira@gmail.com>
2023-05-22 03:09:48 +0100

    🎉 feat: setup prisma

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const PORT = 3333
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello world'
+app.get('/hello', async () => {
+  const users = await prisma.user.findMany()
+  return JSON.stringify(users)
 })
 
 app
