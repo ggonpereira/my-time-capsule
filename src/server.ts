4f1c25e6c77dee4d460b8a6f505e4934fbb51627
Gabriel Pereira <ggonpereira@gmail.com>
2023-06-12 00:17:59 +0100

    🎉 feat: create upload route

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,12 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const JWT_TOKEN = process.env.JWT_SECRET || Math.random().toString()
 
@@ -15,11 +19,18 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
-app.register(authRoutes)
-app.register(memoriesRoutes)
 app.register(jwt, {
   secret: JWT_TOKEN,
 })
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
